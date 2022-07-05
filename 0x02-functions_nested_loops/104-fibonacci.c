aac211e3864d5d9b37bee189626ac3a2fc22aa39
Deborah Thomas <mzdamide@gmail.com>
2022-07-05 15:16:45 -0700

    First 98 fibonnaci series

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
+
 /**
- *
  * main - finds and prints the first 98 Fibonacci numbers,
- *
  * starting with 1 and 2
- *
  * followed by a new line
- *
  * Return: ALways 0 (Success)
- *
  */
 
 int main(void)
