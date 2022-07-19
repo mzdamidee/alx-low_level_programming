e566a3944a7be259829f6d4fb33b015b12a878f4
Deborah Thomas <mzdamide@gmail.com>
2022-07-19 08:47:16 -0700

    a funtion that returns factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,10 +12,9 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else
-		if (n == 0 || n == 1)
-		{
-			return (1);
-		}
-		return (n * factorail(n - 1));
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }
